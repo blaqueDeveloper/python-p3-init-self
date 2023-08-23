d65d7f99018ad4dc68946525b519a3070146724e
Blaque <blaque.develop@gmail.com>
2023-08-23 12:53:50 +0300

    Done with assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def showing_self(self):
+        return self
+    
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog("Fido")
+fido.showing_self()
+print(fido.name)
+fido.get_adopted("Sophie")
+print(fido.owner)
+# print(fido.favorite_toy)
+
+snoopy = Dog("Snoopy", "Tennis Ball")
+# print(snoopy.favorite_toy)
+print(snoopy.name)
